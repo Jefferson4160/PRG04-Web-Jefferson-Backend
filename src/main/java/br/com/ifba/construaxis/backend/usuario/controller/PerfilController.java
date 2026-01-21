e6930be14f0bb8da4e05f38747c73aac6a7ff0fa
Jefferson S Nunes <jefferson.4160@gmail.com>
2026-01-21 15:16:53 -0300

    Ajustes Controller's

--- src/main/java/br/com/ifba/construaxis/backend/usuario/controller/PerfilController.java
+++ src/main/java/br/com/ifba/construaxis/backend/usuario/controller/PerfilController.java
@@ -11,6 +11,7 @@ import java.util.UUID;
 
 @RestController
 @RequestMapping("/perfis")
+@CrossOrigin(origins = "http://localhost:3000")
 public class PerfilController {
 
     private final PerfilRepository perfilRepository;
