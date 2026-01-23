e384dc6aaf18db548b38e7eed8ca344ff26ad6bc
Jefferson S Nunes <jefferson.4160@gmail.com>
2026-01-23 20:49:36 -0300

    Ajuste de nomenclatura para o json na classe fornecedor

--- src/main/java/br/com/ifba/construaxis/backend/parceiro/entity/Fornecedor.java
+++ src/main/java/br/com/ifba/construaxis/backend/parceiro/entity/Fornecedor.java
@@ -1,5 +1,7 @@
 package br.com.ifba.construaxis.backend.parceiro.entity;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import br.com.ifba.construaxis.backend.infrastructure.entity.PersistenceEntity;
 import jakarta.persistence.*;
 import lombok.Data;
@@ -12,6 +14,7 @@ import lombok.NoArgsConstructor;
 public class Fornecedor extends PersistenceEntity {
 
     @Column(name = "nome_fantasia", nullable = false, unique = true)
+    @JsonProperty("nome_fantasia")
     private String nomeFantasia;
 
     @Column(name = "cnpj", unique = true)
