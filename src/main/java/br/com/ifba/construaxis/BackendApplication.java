72c9400874b61f48a562775ca8b879c7e92ab7f9
Jefferson S Nunes <jefferson.4160@gmail.com>
2026-01-23 19:24:28 -0300

    Atualiza aquivo application

--- src/main/java/br/com/ifba/construaxis/BackendApplication.java
+++ src/main/java/br/com/ifba/construaxis/BackendApplication.java
@@ -2,26 +2,12 @@ package br.com.ifba.construaxis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import io.github.cdimascio.dotenv.Dotenv;
-
-
 
 @SpringBootApplication
 public class BackendApplication {
 
-	public static void main(String[] args) {
-        // Carrega o .env ANTES do Spring iniciar
-        Dotenv dotenv = Dotenv.load();
-
-        // Injeta as variáveis no sistema
-        System.setProperty("DB_URL", dotenv.get("DB_URL"));
-        System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
-        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
-
-        // Agora sobe o Spring Boot
+    public static void main(String[] args) {
+        // O Spring Boot lê as variáveis de ambiente automaticamente
         SpringApplication.run(BackendApplication.class, args);
-
-
-	}
-
+    }
 }
